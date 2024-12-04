a7458eb3d72129c8abe984701d1c6c6600b15b45
Asuma Yamada <asuma.a.yamada@dena.com>
2024-12-04 15:03:46 +0900

    fix: use variables to represent rotation

--- src/components/InfiniteGrid.tsx
+++ src/components/InfiniteGrid.tsx
@@ -1,15 +1,16 @@
 import { OrbitControls } from '@react-three/drei'
-import { MOUSE, TOUCH } from 'three'
+import { MOUSE, TOUCH, Euler } from 'three'
 
 export const InfiniteGrid: React.FC = () => {
   const CELL_SIZE = 100000 // > 65535 = Max size for uint16
   const BORDER_COLOR = 0x888888
+  const rotation = new Euler(-Math.PI / 2, 0, 0)
 
   return (
     <>
       <gridHelper
         args={[CELL_SIZE, CELL_SIZE, BORDER_COLOR, BORDER_COLOR]}
-        rotation={[-Math.PI / 2, 0, 0]}
+        rotation={rotation}
         castShadow={false}
       />
       <OrbitControls
