537e2521a8f016a18a583dc5e227db33a76b29b9
Asuma Yamada <asuma.a.yamada@dena.com>
2024-12-04 17:45:50 +0900

    fix: format and lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Canvas } from "@react-three/fiber";
-import { InfiniteGrid } from "./components/InfiniteGrid.tsx";
+import { Canvas } from '@react-three/fiber';
+import { InfiniteGrid } from './components/InfiniteGrid.tsx';
 
 const App = () => {
   return (
-    <Canvas style={{ width: "100vw", height: "100vh" }}>
+    <Canvas style={{ width: '100vw', height: '100vh' }}>
       <InfiniteGrid />
     </Canvas>
   );
